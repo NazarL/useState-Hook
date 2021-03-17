5b9d977d48b99d635249d90c607fa5f2830e4646
NazarL <nazar.lebedenko@gmail.com>
2021-03-17 17:55:23 -0400

    alternative way to work with the useState array

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,18 @@
 import React, { useState } from "react";
 
 function App() {
-  const state = useState(0); // 0 - starting state
-  console.log(state[0]);
-
-  //Destructuring example
-  const [red, green, blue] = [9, 132, 227];
-  console.log(green);
+  //count - name for [0] with initial value
+  //setCount - function to update the initial value
+  const [count, setCount] = useState(0);
+  console.log(count);
 
   function increase() {
-    // count++;
-    // console.log(count);
+    setCount(count + 1);
   }
 
   return (
     <div className="container">
-      <h1>{state[0]}</h1>
+      <h1>{[count]}</h1>
       <button onClick={increase}>+</button>
     </div>
   );
