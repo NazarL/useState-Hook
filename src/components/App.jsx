7956895cb1364a59a0834b1fb90f530d63b46cb6
NazarL <nazar.lebedenko@gmail.com>
2021-03-17 17:45:09 -0400

    add useState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div />;
+  const state = useState(0); // 0 - starting state
+  console.log(state[0]);
+
+  //Destructuring example
+  const [red, green, blue] = [9, 132, 227];
+  console.log(green);
+
+  function increase() {
+    // count++;
+    // console.log(count);
+  }
+
+  return (
+    <div className="container">
+      <h1>{state[0]}</h1>
+      <button onClick={increase}>+</button>
+    </div>
+  );
 }
 
 export default App;
